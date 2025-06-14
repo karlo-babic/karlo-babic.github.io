4f7eb8f4c307e40a80c8041bc074cda39fa97b75
Karlo Babić <39775294+karlo-babic@users.noreply.github.com>
2025-06-14 17:59:39 +0200

    Add files via upload

--- scripts/programs/grid-simulation-base.js
+++ scripts/programs/grid-simulation-base.js
@@ -191,7 +191,7 @@ class GridSimulation {
     randomizeGrid() {
         for (let y = 0; y < this.rows; y++) {
             for (let x = 0; x < this.cols; x++) {
-                this.grid[y][x] = Math.random() > 0.9 ? 1 : 0;
+                this.grid[y][x] = Math.random() > 0.95 ? 1 : 0;
             }
         }
     }
